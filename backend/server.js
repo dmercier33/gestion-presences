8a5e646862df06cd05458f2827c6bda76d6e9d6c
dmercier33 <denism33@gmail.com>
2026-07-06 16:04:28 +0200

    on tourne en rond !

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,10 @@ app.use(express.json());
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// servir le frontend
-app.use(express.static(path.join(__dirname, "../frontend")));
+app.get("/scanner.html", (req, res) => {
+  console.log("HIT scanner.html route");
+  res.send("ROUTE OK BUT STATIC BYPASSED");
+});
 
 app.get("/test-front", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/scanner.html"));
