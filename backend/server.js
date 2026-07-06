260af8aca810380ca50d358ff02e031173c86765
dmercier33 <denism33@gmail.com>
2026-07-06 14:29:52 +0200

    fix path static frontend

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // servir le frontend
-app.use(express.static(path.join(__dirname, "frontend")));
+app.use(express.static(path.join(__dirname, "../frontend")));
 
 // 🔗 SUPABASE
 const supabase = createClient(
