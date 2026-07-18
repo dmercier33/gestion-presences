a9c6f4aae603ed8095b3c79a5096c1c6447041fa
dmercier33 <denism33@gmail.com>
2026-07-18 06:11:10 +0200

    allo houston

--- backend/app.js
+++ backend/app.js
@@ -38,6 +38,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log("REQUETE RECUE :", req.method, req.url);
+  next();
+});
+
 // servir frontend
 const frontendPath = path.resolve(process.cwd(), "../frontend");
 app.use(express.static(frontendPath));
